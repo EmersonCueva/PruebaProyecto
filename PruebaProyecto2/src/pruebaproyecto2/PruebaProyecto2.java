8fb392c27d97b025c19423522db9a25675d0d4ee
Emerson Alexander Cueva Carbajal <CC21045@ti.ues.edu.sv>
2022-10-06 23:52:01 -0600

    Subiendo primeros archivos

---
+++ PruebaProyecto2/src/pruebaproyecto2/PruebaProyecto2.java
@@ -0,0 +1,13 @@
+
+package pruebaproyecto2;
+
+
+public class PruebaProyecto2 {
+
+   
+    public static void main(String[] args) {
+    
+        System.out.println("Prueba proyecto 2");
+    }
+
+}
