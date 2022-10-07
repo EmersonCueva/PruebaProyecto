9d8e6256659b310b0fa0a37004f235e7fb05296d
Emerson Alexander Cueva Carbajal <CC21045@ti.ues.edu.sv>
2022-10-07 00:14:03 -0600

    Perpectiva de compañeros

--- PruebaProyecto2/src/pruebaproyecto2/PruebaProyecto2.java
+++ PruebaProyecto2/src/pruebaproyecto2/PruebaProyecto2.java
@@ -8,6 +8,7 @@ public class PruebaProyecto2 {
     public static void main(String[] args) {
     
         System.out.println("Prueba proyecto 2");
+        System.out.println("Prueba desde perspectiva compañeros");
     }
 
 }
